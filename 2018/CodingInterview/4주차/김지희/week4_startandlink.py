aa53aeb1fc14475c4b3e2ce997e239e5506e3b18
kimjihee <aszx1220@gmail.com>
2018-01-27 19:40:34 +0900

    mkdir_week4

---
+++ 2018/CodingInterview/4주차/김지희/week4_startandlink.py
@@ -0,0 +1,43 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+from itertools import permutations
+from itertools import combinations
+
+
+# In[ ]:
+
+
+member= int(input())
+score_list =  [list(map(int, input().split())) for i in range(member)]
+
+
+# In[ ]:
+
+
+comb_of_member = list(combinations([i for i in range(member)],member//2))
+res = (list(zip(comb_of_member, comb_of_member[::-1])))
+result_list =[]
+for comb  in res:
+    start, link = comb
+    total_score_start = 0
+    total_score_link = 0
+    start2pack = list(permutations(start,2))
+    link2pack = list(permutations(link,2))
+    for i in range(len(start2pack)):
+        first_s, second_s = start2pack[i]
+        total_score_start += score_list[first_s][second_s]
+        first_l, second_l = link2pack[i]
+        total_score_link += score_list[first_l][second_l]
+    result_list.append(abs(total_score_link-total_score_start))
+print(min(result_list))    
+
+
+# In[ ]:
+
+
+
+
